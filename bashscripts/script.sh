79c88ddeb79eca1c1f94cb66b2323112320d03be
xubzero <allannjugush@gmail.com>
2022-01-28 11:05:29 +0300

    New update

--- bashscripts/script.sh
+++ bashscripts/script.sh
@@ -256,7 +256,7 @@ searchpacketscriteria(){
 
 	else
 		echo -e "$fail Invalid sign: Make sure the value is in the format \`VALUE_HERE\` $end"
-		echo -e "$fail Invalid sign: For bytes and packets : write in the format PACKETS > \`10\` or  $end"
+		echo -e "$fail Invalid sign: For bytes and packets : write in the format PACKETS > \`10\` or BYTES > \`10\` $end"
 		exit
 	fi
 
