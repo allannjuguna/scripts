57945709ec7baa447c3760eebb573c6d0a8ded40
xubzero <allannjugush@gmail.com>
2022-02-02 14:03:08 +0300

    New update

--- bashscripts/script3.sh
+++ bashscripts/script3.sh
@@ -22,13 +22,6 @@
 Color_Off='\033[0m'       # Text Reset
 
 # Regular Colors
-black='\033[0;30m'        # Black
-red='\033[0;31m'          # Red
-green='\033[0;32m'        # Green
-yellow='\033[0;33m'       # Yellow
-blue='\033[0;34m'         # Blue
-purple='\033[0;35m'       # Purple
-cyan='\033[0;36m'         # Cyan
 white='\033[0;37m'        # White
 
 ENTRY=">>"
@@ -41,15 +34,6 @@ PacketCount=0
 # DELETE THIS FUCTIONS AFTER USE
 State="log"
 
-debugger(){
-	if [[ $State == "log" ]]
-	then
-		echo -e " [debug] ${1}"
-	else
-		# Do nothing
-		true 
-	fi
-}
 
 # Functions start here
 breakpoint(){
